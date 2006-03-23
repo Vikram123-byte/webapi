7f9a65117ebb88f59d923edfd3705be04781899a
mstachow <mstachow>
2006-03-23 22:15:19 +0000

    2006-03-23  Maciej Stachowiak  <mjs@apple.com>
            - rearrange to be compatible with how ReTest does things
    
            * ecmascript/browsing-contexts/DocumentWindow-defaultView.js: Removed.
            * ecmascript/browsing-contexts/Window-document.js: Removed.
            * ecmascript/browsing-contexts/Window-self.js: Removed.
            * ecmascript/browsing-contexts/Window-window.js: Removed.
            * publish/html/browsing-contexts/DocumentWindow-defaultView.html: Removed.
            * publish/html/browsing-contexts/Window-document.html: Removed.
            * publish/html/browsing-contexts/Window-self.html: Removed.
            * publish/html/browsing-contexts/Window-window.html: Removed.
            * publish/html/ecmascript/browsing-contexts/DocumentWindow-defaultView.html: Added.
            * publish/html/ecmascript/browsing-contexts/Window-document.html: Added.
            * publish/html/ecmascript/browsing-contexts/Window-self.html: Added.
            * publish/html/ecmascript/browsing-contexts/Window-window.html: Added.
            * publish/shared/browsing-contexts/DocumentWindow-defaultView.js: Removed.
            * publish/shared/browsing-contexts/Window-document.js: Removed.
            * publish/shared/browsing-contexts/Window-self.js: Removed.
            * publish/shared/browsing-contexts/Window-window.js: Removed.
            * publish/shared/ecmascript/browsing-contexts/DocumentWindow-defaultView.js: Added.
            * publish/shared/ecmascript/browsing-contexts/Window-document.js: Added.
            * publish/shared/ecmascript/browsing-contexts/Window-self.js: Added.
            * publish/shared/ecmascript/browsing-contexts/Window-window.js: Added.
            * publish/shared/js-test-xhtml.js: Removed.
            * publish/svg/browsing-contexts/DocumentWindow-defaultView.svg: Removed.
            * publish/svg/browsing-contexts/Window-document.svg: Removed.
            * publish/svg/browsing-contexts/Window-self.svg: Removed.
            * publish/svg/browsing-contexts/Window-window.svg: Removed.
            * publish/svg/ecmascript/browsing-contexts/DocumentWindow-defaultView.svg: Added.
            * publish/svg/ecmascript/browsing-contexts/Window-document.svg: Added.
            * publish/svg/ecmascript/browsing-contexts/Window-self.svg: Added.
            * publish/svg/ecmascript/browsing-contexts/Window-window.svg: Added.
            * publish/xhtml/browsing-contexts/DocumentWindow-defaultView.xhtml: Removed.
            * publish/xhtml/browsing-contexts/Window-document.xhtml: Removed.
            * publish/xhtml/browsing-contexts/Window-self.xhtml: Removed.
            * publish/xhtml/browsing-contexts/Window-window.xhtml: Removed.
            * publish/xhtml/ecmascript/browsing-contexts/DocumentWindow-defaultView.xhtml: Added.
            * publish/xhtml/ecmascript/browsing-contexts/Window-document.xhtml: Added.
            * publish/xhtml/ecmascript/browsing-contexts/Window-self.xhtml: Added.
            * publish/xhtml/ecmascript/browsing-contexts/Window-window.xhtml: Added.
            * scripts/TEMPLATE.html: Removed.
            * scripts/TEMPLATE.svg: Removed.
            * scripts/TEMPLATE.xhtml: Removed.
            * scripts/generate-tests: Removed.
            * source/ecmascript/browsing-contexts/DocumentWindow-defaultView.js: Added.
            * source/ecmascript/browsing-contexts/Window-document.js: Added.
            * source/ecmascript/browsing-contexts/Window-self.js: Added.
            * source/ecmascript/browsing-contexts/Window-window.js: Added.

--- WindowTestSuite/publish/shared/js-test-xhtml.js
+++
@@ -1,8 +0,0 @@
-function debug(msg)
-{
-    var span = document.createElementNS("http://www.w3.org/1999/xhtml", "span");
-    alert(span);
-    span.innerHTML = msg + '<br/>';
-    document.getElementById("console").appendChild(span);
-}
-
