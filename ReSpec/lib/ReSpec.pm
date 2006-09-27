d8a5ba15ca6f99d676a02f99e4fc55f2e3567df9
rberjon <rberjon>
2006-09-27 11:07:40 +0000

    new version

--- ReSpec/lib/ReSpec.pm
+++ ReSpec/lib/ReSpec.pm
@@ -16,7 +16,7 @@ use warnings;
 #     sequence of test writing
 ####
 
-our $VERSION = '0.55';
+our $VERSION = '0.56';
 
 use XML::LibXML                 qw();
 use XML::LibXML::XPathContext   qw();
