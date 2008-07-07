9ba5d7a823cfcec4ef9c0b63393b3704d71d53d8
lhunt <lhunt>
2008-07-07 14:59:06 +0000

    Added placeholder test files

---
+++ selectors-api-testsuite/test-exceptions.js
@@ -0,0 +1,40 @@
+/**
+ * This set tests that object returned by querySelectorAll is a static NodeList
+ * that correctly implements the NodeList interface.
+ */
+var tests = [
+function() {
+	
+	return true;
+},
+function() {
+	return true;	
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+}
+];
---
+++ selectors-api-testsuite/test-queryselectorall-return-values.js
@@ -0,0 +1,39 @@
+/**
+ * This set tests that the methods return the correct matching element
+ */
+var tests = [
+function() {
+	
+	return true;
+},
+function() {
+	return true;	
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+},
+function() {
+	return true;
+}
+];
