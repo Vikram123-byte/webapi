84f745c86edf0f00a952813da39c663e34ecf704
mike <mike>
2011-02-27 03:51:52 +0000

    added bibref file for Feature Permissions draft.

---
+++ WebNotifications/respec2/w3c/bibref/PERMISSIONS.js
@@ -0,0 +1,5 @@
+define({
+   "html": "John Gregg. <a href=\"http://dev.w3.org/2006/webapi/WebNotifications/publish/FeaturePermissions.html\"><cite>Feature Permissions</cite></a> 12 October 2010. Editor’s Draft. URL: <a href=\"http://dev.w3.org/2006/webapi/WebNotifications/publish/FeaturePermissions.html">http://dev.w3.org/2006/webapi/WebNotifications/publish/FeaturePermissions.html</a> ",
+   "id": "PERMISSIONS"
+}
+);
